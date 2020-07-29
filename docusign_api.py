b5126659237d0a41595f59a63db645df9cdfad3f
LucasPanao <panaotrabalho@gmail.com>
2020-07-29 13:53:56 -0300

    Capturando dados do recebedor
    Foi criado um outro loop para obter os dados do recebedor.
    
    A requisição foi colocada dentro de uma função para facilitar a leitura.

--- docusign_api.py
+++ docusign_api.py
@@ -3,13 +3,17 @@ import requests
 import yaml
 import json
 
+def request(url,hdr):
+    global data
+    response = requests.get(url, headers = hdr)
+    data = response.json()
+
 with open('config.yml') as c:
     config = yaml.safe_load(c)
 url = config['my-config']['url']
 hdr = config['my-config']['token']
 
-response = requests.get(url, headers = hdr)
-data = response.json()
+request(url,hdr)
 
 for envlp in data['envelopes']:
     envlpId = (envlp['envelopeId'])
@@ -17,4 +21,16 @@ for envlp in data['envelopes']:
     envlpCompl = (envlp['completedDateTime'])
 print(envlpId,envlpSts,envlpCompl)
 
-with open('info_contrato.json', 'w') as f:json.dump(data, f)
+url = url.split('?') # divide os parametros passados na URL 
+
+url = url[0] + '/' + envlpId + '/recipients' #cria a url para encontrar os dados do recebedor
+
+request(url,hdr)
+
+for signers in data['signers']:
+    s_name = (signers['name'])
+    s_email = (signers['email'])
+    s_recipientId = (signers['recipientId'])
+print(s_name,s_email,s_recipientId)
+
+with open('contrato.json', 'w') as f:json.dump(data, f)
