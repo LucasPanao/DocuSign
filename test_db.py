26e4ba9105bd3b91610d24a46284b044f90e5cb4
LucasPanao <panaotrabalho@gmail.com>
2020-07-29 14:24:19 -0300

    Inserindo valores dentro do banco de dados
    A inserção de valores agora é possível ao nosso banco SQL server.

--- test_db.py
+++ test_db.py
@@ -8,8 +8,10 @@ server = db_config['db-config']['server']
 database = db_config['db-config']['database']
 username = db_config['db-config']['username']
 password = db_config['db-config']['password']
+valor_id = '12345'
 
 def retornar_conexao_sql():
+    global conexao
     #username = ""
     #password = ""
     #string_conexao = 'Driver={ODBC DRIVER 17 for SQL Server};Server='+server+';Database='+database+';UID='+username+';PWD='+ password
@@ -18,7 +20,10 @@ def retornar_conexao_sql():
     return conexao.cursor()
 
 cursor = retornar_conexao_sql()
-cursor.execute("SELECT * FROM tb_alunos")
-row = cursor.fetchone()
-if row:
-    print(row)
+cursor.execute("SELECT * FROM Integra_DocuSign")
+cursor.execute('''
+                INSERT INTO Integra_DocuSign (id_envelope)
+                VALUES
+                (?)
+                ''', valor_id)
+conexao.commit()
