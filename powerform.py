ab3063206044319a0a26d8a6e872ee3f9ecd6f0f
Lucas Ribeiro Panão <panaotrabalho@gmail.com>
2020-08-18 16:28:37 -0300

    Removendo funções não utilizadas e criando novas.
    também foi implementado a chamada para as "tabs"

--- powerform.py
+++ powerform.py
@@ -4,8 +4,6 @@ import yaml
 import json
 import pyodbc
 from docusign_esign import EnvelopesApi, EnvelopeDefinition, TemplateRole
-from consts import pattern
-from flask import request, session
 import base64
 
 
@@ -14,58 +12,34 @@ def requested(url,hdr):
     global data
     response = requests.get(url, headers = hdr)
     data = response.json()
+    with open('envelope.json', 'w') as f:json.dump(data, f)
 
-def get_args(*kwargs):
-        global args
-        global envelope_args
-        envelope_args = {
-            "signer_email": signer_email,
-            "signer_name": signer_name,
-            "cc_email": cc_email,
-            "cc_name": cc_name,
-            "template_id": template_id
-        }
-        args = {
-            "account_id": signer_client_id,
-            "base_path": basepath,
-            "access_token": token_only,
-            "envelope_args": envelope_args
-        }
-        return args
-
-def make_envelope(args):
-        """
-        Creates envelope
-        args -- parameters for the envelope:
-        signer_email, signer_name, signer_client_id
-        returns an envelope definition
-        """
-        global envelope_definition
-        # create the envelope definition
-        envelope_definition = EnvelopeDefinition(
-            status="sent",  # requests that the envelope be created and sent.
-            template_id=args["template_id"]
-        )
-        # Create template role elements to connect the signer and cc recipients
-        # to the template
-        signer = TemplateRole(
-            email=args["signer_email"],
-            name=args["signer_name"],
-            role_name="signer"
-        )
-        # Create a cc template role.
-        cc = TemplateRole(
-            email=args["cc_email"],
-            name=args["cc_name"],
-            role_name="cc"
-        )
-
-        # Add the TemplateRole objects to the envelope object
-        envelope_definition.template_roles = [signer, cc]
-        return envelope_definition
-
+def create_envelope(*kwargs):
+  global dataEnv
+  dataEnv = {
+    "documents": [
+      {
+        "documentBase64": ""+pdf_64+"",
+        "documentId": "99428916",
+        "name": "CONTRATO OBJ API PYTHON WITH VAR"
+      }
+    ],
+    "emailSubject": ""+signer_email+"",
+    "status": ""+status+"",
+    "templateId": ""+template_id+"",
+    "templateRoles": [
+      {
+        "email": ""+cc_email+"",
+        "name": ""+cc_name+"",
+        "roleName": "Objetivo"
+      }
+    ]
+  }
+  return dataEnv
 #### END FUCTIONS   
 
+
+#### CONFIG
 with open('config.yml') as c:
     config = yaml.safe_load(c)
 url = config['my-config']['url']
@@ -79,35 +53,17 @@ signer_email = config['signer']['email']
 signer_name = config['signer']['name']
 cc_email = config['cc']['email']
 cc_name = config['cc']['name']
+#### END CONFIG
+
 
 ### CONVERTING PDF INTO BASE64 FILE
 with open("contrato-objetivo-2.pdf", "rb") as pdf_file:
     pdf_64 = base64.b64encode(pdf_file.read())
 pdf_64 = pdf_64.decode("UTF-8")
 
-dataEnv = {
-  "documents": [
-    {
-      "documentBase64": ""+pdf_64+"",
-      "documentId": "99428916",
-      "name": "CONTRATO OBJ API PYTHON WITH VAR"
-    }
-  ],
-  "emailSubject": ""+signer_email+"",
-  "status": ""+status+"",
-  "templateId": ""+template_id+"",
-  "templateRoles": [
-    {
-      "email": ""+cc_email+"",
-      "name": ""+cc_name+"",
-      "roleName": "Objetivo"
-    }
-  ]
-}
-
-
-kwargs = (signer_email,signer_name,cc_email,cc_name,template_id,signer_client_id,basepath,token_only)
-get_args(kwargs)
+### CREATING ENVELOPE
+kwargs = (pdf_64,signer_email,status,template_id,cc_email,cc_name)
+create_envelope(kwargs)
 
 r = requests.post(url,json= dataEnv, headers = hdr)
 print(r.json())
@@ -115,6 +71,7 @@ data = r.json()
 envelopeId = data['envelopeId']
 print(envelopeId)
 
+### CREATING ENVELOPE_VIEW 
 dataView = {
   "envelopeId" : ""+envelopeId+"",
   "returnUrl" : "https://google.com"
@@ -123,5 +80,9 @@ dataView = {
 url_view = 'https://demo.docusign.net/restapi/v2.1/accounts/{0}/views/console'.format(signer_client_id) 
 r = requests.post(url_view,json = dataView, headers = hdr)
 data = r.json()
-envelopeId = data['url']
-print(envelopeId)
+envelopeUrl = data['url']
+print(envelopeUrl)
+
+### COLLETING TABS FROM ACC 
+url = 'https://demo.docusign.net/restapi/v2.1/accounts/{0}/tab_definitions'.format(signer_client_id)
+requested(url,hdr)
