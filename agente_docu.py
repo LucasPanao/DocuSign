b3876dbfee9e34dc75baa677a77dbaed04dbbedc
Lucas Ribeiro Panão <panaotrabalho@gmail.com>
2020-09-15 10:00:32 -0300

    Nova consulta para preencher o termo aditivo

--- agente_docu.py
+++ agente_docu.py
@@ -7,9 +7,12 @@ i = 0
 ## INICIA OS ARRAYS COM AS INFOS DO BANCO PARA O TEMPLATE
 start_var.start_variables_template()
 
+## INICIA OS ARRAYS DO ADITIVO 
+start_var.start_variables_template_aditivo()
+
 length = len(db_docu.rows)
 
-while i < length:
+while i < 1:
     ## INSERE AS INFOS DO BD NO TEMPLATE
     template.update_template(i)
 
--- template.py
+++ template.py
@@ -280,6 +280,338 @@ def update_template(i):
           "tabLabel": "Ano",
           "value": ""+str(date.today().year)+""
         },
+        {
+          "tabId": "99d1e6ad-399b-46c2-8349-66610cf86bfa",
+          "tabLabel": "ANOLETIVO",
+          "value": ""+start_var.ANOLETIVO[i]+""
+        },
+        ## Termo Aditivo ##
+        {
+          "tabId": "e5e2c3a3-f830-4e04-9ccb-f78680d63754",
+          "tabLabel": "Unidade",
+          "value": ""+start_var.UNIDADE_AD[i]+""
+        },
+        {
+          "tabId": "7c32521a-2a9d-4b2f-b41e-dd736abb04fe",
+          "tabLabel": "Curso",
+          "value": ""+start_var.CURSO_AD[i]+""
+        },
+        {
+          "tabId": "023223fb-3062-486b-b368-8a1b03383c09",
+          "tabLabel": "RA",
+          "value": ""+start_var.RA_AD[i]+""
+        },
+        {
+          "tabId": "57039aff-fe69-49d6-b469-3659ec4833e9",
+          "tabLabel": "Nome Aluno",
+          "value": ""+start_var.NOME_AD[i]+""
+        },
+        {
+          "tabId": "b59962f2-3d31-47da-ad25-70d12136fa91",
+          "tabLabel": "Dia",
+          "value": ""+str(date.today().day)+""
+        },
+        {
+          "tabId": "2b0dcb22-1948-44ae-9439-c4a6280a4a50",
+          "tabLabel": "Mes",
+          "value": ""+str(nome_mes[date.today().month])+""
+        },
+        {
+          "tabId": "1a5d1b01-8fc3-4c93-bdd4-d5f222f6341f",
+          "tabLabel": "Ano",
+          "value": ""+str(date.today().year)+""
+        },
+        {
+          "tabId": "8db17200-a55d-4d8c-a95e-380522f821fa",
+          "tabLabel": "Ano",
+          "value": ""+str(date.today().year)+""
+        },
+        {
+          "tabId": "a3ae4406-b400-43b1-90e1-f7ea7d05f959",
+          "tabLabel": "Ano",
+          "value": ""+str(date.today().year)+""
+        },
+        {
+          "tabId": "5dea020e-c42d-4697-9925-dd4c446edafc",
+          "tabLabel": "Nome Resp. Financeiro",
+          "value": ""+start_var.RESP_AD[i]+""
+        },
+        {
+          "tabId": "36a9fe99-189b-4b33-8644-d0ddb59c52f4",
+          "tabLabel": "RG",
+          "value": ""+start_var.RG_AD[i]+""
+        },
+        {
+          "tabId": "3224c534-c673-427c-bdd8-1ec870ac67c6",
+          "tabLabel": "CPF",
+          "value": ""+start_var.CPF_AD[i]+""
+        },
+        {
+          "tabId": "3e920a95-fad0-4d77-b9a6-22064e11dd1f",
+          "tabLabel": "CodContrato",
+          "value": ""+start_var.CODCONTRATO_AD[i]+""
+        },
+        ### MATRIZ TERMO ADITIVO ###
+        {
+          "tabId": "901b0d1d-4cea-46e9-a894-0f4b7a81bb0e",
+          "tabLabel": "BOLSA1",
+          "value": ""+start_var.BOLSA1[i]+""
+        },
+        {
+          "tabId": "e936594c-6ff3-48e8-bac1-b636353d644e",
+          "tabLabel": "BOLSA2",
+          "value": ""+start_var.BOLSA2[i]+""
+        },
+        {
+          "tabId": "500efe86-17cc-4088-a2a0-57fec6487270",
+          "tabLabel": "BOLSA3",
+          "value": ""+start_var.BOLSA3[i]+""
+        },
+        {
+          "tabId": "70f5a0e2-0015-44c3-8d3f-ebf36b62f740",
+          "tabLabel": "BOLSA4",
+          "value": ""+start_var.BOLSA4[i]+""
+        },
+        {
+          "tabId": "49a7b18f-3a20-4e7f-b122-243a1450e257",
+          "tabLabel": "BOLSA5",
+          "value": ""+start_var.BOLSA5[i]+""
+        },
+        {
+          "tabId": "27d18d83-d52e-4fd3-8840-b860497f7b6f",
+          "tabLabel": "BOLSA6",
+          "value": ""+start_var.BOLSA6[i]+""
+        },
+        {
+          "tabId": "8b55a016-74f8-4478-bfc2-929d47818a64",
+          "tabLabel": "BOLSA7",
+          "value": ""+start_var.BOLSA7[i]+""
+        },
+        {
+          "tabId": "21906dd0-ef2c-4618-843c-06128a3caea5",
+          "tabLabel": "BOLSA8",
+          "value": ""+start_var.BOLSA8[i]+""
+        },
+        {
+          "tabId": "f55fc742-7d9f-46c7-af1f-ecd18a0c4997",
+          "tabLabel": "BOLSA9",
+          "value": ""+start_var.BOLSA9[i]+""
+        },
+        {
+          "tabId": "74e924bb-4099-494d-a6ba-ad14064d171b",
+          "tabLabel": "BOLSA10",
+          "value": ""+start_var.BOLSA10[i]+""
+        },
+        {
+          "tabId": "2903ff1b-0079-42c6-b793-9cec5c08f2db",
+          "tabLabel": "BOLSA11",
+          "value": ""+start_var.BOLSA11[i]+""
+        },
+        {
+          "tabId": "406385e9-f0de-4a98-bf34-77857be762b4",
+          "tabLabel": "BOLSA12",
+          "value": ""+start_var.BOLSA12[i]+""
+        },
+        {
+          "tabId": "ed1c882b-bb1a-42c2-8c80-859b766cbcb9",
+          "tabLabel": "BOLSA13",
+          "value": ""+start_var.BOLSA13[i]+""
+        },
+        {
+          "tabId": "0cd339ec-bedb-417c-99c4-948f5c67aaa9",
+          "tabLabel": "VALB1",
+          "value": ""+start_var.VALB[i]+""
+        },
+        {
+          "tabId": "8d2e806f-1940-4842-bf3a-56404b0462ab",
+          "tabLabel": "VALB2",
+          "value": ""+start_var.VALB2[i]+""
+        },
+        {
+          "tabId": "f108764e-d3f8-47fe-b128-3f7fcf545161",
+          "tabLabel": "VALB3",
+          "value": ""+start_var.VALB3[i]+""
+        },
+        {
+          "tabId": "a287e0e7-9f52-4bff-ab7d-6a56ec1281f7",
+          "tabLabel": "VALB4",
+          "value": ""+start_var.VALB4[i]+""
+        },
+        {
+          "tabId": "7d7df6cf-b37d-4494-b0f6-2f653529cb81",
+          "tabLabel": "VALB5",
+          "value": ""+start_var.VALB5[i]+""
+        },
+        {
+          "tabId": "7d66cffe-3d00-417e-833b-d8acbba22e71",
+          "tabLabel": "VALB6",
+          "value": ""+start_var.VALB6[i]+""
+        },
+        {
+          "tabId": "76264a1c-5ea8-40b1-82a2-d298784778b6",
+          "tabLabel": "VALB7",
+          "value": ""+start_var.VALB7[i]+""
+        },
+        {
+          "tabId": "0b073997-1969-4b20-9684-cd2b5be94942",
+          "tabLabel": "VALB8",
+          "value": ""+start_var.VALB8[i]+""
+        },
+        {
+          "tabId": "14cfd4df-7b49-44f2-a144-b50af3365830",
+          "tabLabel": "VALB9",
+          "value": ""+start_var.VALB9[i]+""
+        },
+        {
+          "tabId": "bb2d71f4-2b5d-4dee-a2e5-a7e35543702a",
+          "tabLabel": "VALB10",
+          "value": ""+start_var.VALB10[i]+""
+        },
+        {
+          "tabId": "254ee055-faff-4363-a2fe-898484dded2d",
+          "tabLabel": "VALB11",
+          "value": ""+start_var.VALB11[i]+""
+        },
+        {
+          "tabId": "437d73a2-8da8-40a6-a250-3e1a0432d85a",
+          "tabLabel": "VALB12",
+          "value": ""+start_var.VALB12[i]+""
+        },
+        {
+          "tabId": "b7896fa3-f721-413f-b5f0-a07022ff918c",
+          "tabLabel": "VALB13",
+          "value": ""+start_var.VALB13[i]+""
+        },
+        {
+          "tabId": "8152b4be-3dfb-4799-a65a-6749b3cd1e80",
+          "tabLabel": "VALDESC1",
+          "value": ""+start_var.VALDESC1[i]+""
+        },
+        {
+          "tabId": "c4488ee6-a772-4202-9bbe-219c1feeedf9",
+          "tabLabel": "VALDESC2",
+          "value": ""+start_var.VALDESC2[i]+""
+        },
+        {
+          "tabId": "c30f9ef1-78c9-416b-b347-2a3b08af1be4",
+          "tabLabel": "VALDESC3",
+          "value": ""+start_var.VALDESC3[i]+""
+        },
+        {
+          "tabId": "cb72cb03-5445-4ece-88ae-e1a9ca2ecb7e",
+          "tabLabel": "VALDESC4",
+          "value": ""+start_var.VALDESC4[i]+""
+        },
+        {
+          "tabId": "c9c06e2d-e33a-4ad8-b0a3-ad7e2d939f3f",
+          "tabLabel": "VALDESC5",
+          "value": ""+start_var.VALDESC5[i]+""
+        },
+        {
+          "tabId": "2b7f53d6-222f-4d33-ae1f-4087d4d03402",
+          "tabLabel": "VALDESC6",
+          "value": ""+start_var.VALDESC6[i]+""
+        },
+        {
+          "tabId": "ff590ac1-9ae5-45bc-8105-1a8509bc7934",
+          "tabLabel": "VALDESC7",
+          "value": ""+start_var.VALDESC7[i]+""
+        },
+        {
+          "tabId": "a391b47c-0f95-4cd2-9acd-2a0794d0c8b4",
+          "tabLabel": "VALDESC8",
+          "value": ""+start_var.VALDESC8[i]+""
+        },
+        {
+          "tabId": "1144f8b1-b5da-43b2-898b-bec44ee28c71",
+          "tabLabel": "VALDESC9",
+          "value": ""+start_var.VALDESC9[i]+""
+        },
+        {
+          "tabId": "96cba413-6a34-47bd-8985-c0bc1df1da71",
+          "tabLabel": "VALDESC10",
+          "value": ""+start_var.VALDESC10[i]+""
+        },
+        {
+          "tabId": "e153237b-03a2-4b32-8d13-816779bdc65b",
+          "tabLabel": "VALDESC11",
+          "value": ""+start_var.VALDESC11[i]+""
+        },
+        {
+          "tabId": "eaf8899c-f4a3-4825-8f6b-b1ed7f8b73c8",
+          "tabLabel": "VALDESC12",
+          "value": ""+start_var.VALDESC12[i]+""
+        },
+        {
+          "tabId": "69868a88-9a6a-41fe-a721-073ddc3c952f",
+          "tabLabel": "VALDESC13",
+          "value": ""+start_var.VALDESC13[i]+""
+        },
+        {
+          "tabId": "9ccf14d8-2026-4d37-90e0-fcaf442ebe4b",
+          "tabLabel": "VALLQ1",
+          "value": ""+start_var.VALLQ1[i]+""
+        },
+        {
+          "tabId": "25380beb-e23a-4d86-bad7-4d9d259dbc97",
+          "tabLabel": "VALLQ2",
+          "value": ""+start_var.VALLQ2[i]+""
+        },
+        {
+          "tabId": "4321ac0b-fca0-4df0-886c-ad3a8f8c49a6",
+          "tabLabel": "VALLQ3",
+          "value": ""+start_var.VALLQ3[i]+""
+        },
+        {
+          "tabId": "3413cae1-22f6-45c6-a76c-1e2a6154c630",
+          "tabLabel": "VALLQ4",
+          "value": ""+start_var.VALLQ4[i]+""
+        },
+        {
+          "tabId": "e146bfb5-5176-40d0-9441-709446615e42",
+          "tabLabel": "VALLQ5",
+          "value": ""+start_var.VALLQ5[i]+""
+        },
+        {
+          "tabId": "e518d37d-37ba-4dbb-98ca-2ceb15cb0800",
+          "tabLabel": "VALLQ6",
+          "value": ""+start_var.VALLQ6[i]+""
+        },
+        {
+          "tabId": "69d9a206-735f-4a48-a892-a46a4ea52968",
+          "tabLabel": "VALLQ7",
+          "value": ""+start_var.VALLQ7[i]+""
+        },
+        {
+          "tabId": "521fcb21-0a9d-4e6e-82e3-64c98fd20e81",
+          "tabLabel": "VALLQ8",
+          "value": ""+start_var.VALLQ8[i]+""
+        },
+        {
+          "tabId": "5e9cebf7-334f-46fe-9354-e73e1ce08175",
+          "tabLabel": "VALLQ9",
+          "value": ""+start_var.VALLQ9[i]+""
+        },
+        {
+          "tabId": "453eb037-3652-497a-acd9-0f5ac74d4786",
+          "tabLabel": "VALLQ10",
+          "value": ""+start_var.VALLQ10[i]+""
+        },
+        {
+          "tabId": "16c10f24-5cf4-4279-9a0a-e24f54ffae4b",
+          "tabLabel": "VALLQ11",
+          "value": ""+start_var.VALLQ11[i]+""
+        },
+        {
+          "tabId": "3227af0e-7a19-4304-8d36-2bb66575e15c",
+          "tabLabel": "VALLQ12",
+          "value": ""+start_var.VALLQ12[i]+""
+        },
+        {
+          "tabId": "745c8fd2-7c5b-4cfb-9139-bf0ce2bcf713",
+          "tabLabel": "VALLQ13",
+          "value": ""+start_var.VALLQ13[i]+""
+        },
       ]
     }
   return dataTemplate
